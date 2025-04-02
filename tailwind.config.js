cc5d26ae8bdba964f01353d79b8ae3b4018f751a
Cyclopsman <151868524+Cyclopsman@users.noreply.github.com>
2025-04-02 14:58:57 +0000

    Add tailwind.config.js 1743605936037

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './App.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'blue-900': '#1e3a8a',
+        'green-500': '#10b981',
+        'gray-50': '#f9fafb',
+        'gray-800': '#1f2937'
+      }
+    },
+  },
+  plugins: [],
+}
